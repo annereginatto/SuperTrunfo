eadcc1968edabd286101528d548e3d01923fcfed
Anne Reginatto <73719693+annereginatto@users.noreply.github.com>
2021-09-28 14:42:49 -0300

    Adicionando arquivos no GitHub

---
+++ script.js
@@ -0,0 +1,634 @@
+let carta1 = {
+  nome: "Brasil",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/250px-Flag_of_Brazil.svg.png",
+  atributos: {
+    área: 8515767,
+    população: 210147125,
+    densidade: 25,
+    IDH: 0.765,
+    renda: 7010
+  }
+};
+
+let carta2 = {
+  nome: "Itália",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/250px-Flag_of_Italy.svg.png",
+  atributos: {
+    área: 301340,
+    população: 60317116,
+    densidade: 201.3,
+    IDH: 0.892,
+    renda: 34997
+  }
+};
+
+let carta3 = {
+  nome: "Estados Unidos",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/250px-Flag_of_the_United_States.svg.png",
+  atributos: {
+    área: 9833520,
+    população: 331449281,
+    densidade: 33.6,
+    IDH: 0.926,
+    renda: 68309
+  }
+};
+
+let carta4 = {
+  nome: "Canadá",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/250px-Flag_of_Canada_%28Pantone%29.svg.png",
+  atributos: {
+    área: 9984670,
+    população: 38131104,
+    densidade: 3.92,
+    IDH: 0.929,
+    renda: 49222
+  }
+};
+
+let carta5 = {
+  nome: "Austrália",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/250px-Flag_of_Australia_%28converted%29.svg.png",
+  atributos: {
+    área: 7692024,
+    população: 25871100,
+    densidade: 3.4,
+    IDH: 0.944,
+    renda: 62728
+  }
+};
+
+let carta6 = {
+  nome: "Alemanha",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/250px-Flag_of_Germany.svg.png",
+  atributos: {
+    área: 357022,
+    população: 83190556,
+    densidade: 232,
+    IDH: 0.947,
+    renda: 51860
+  }
+};
+
+let carta7 = {
+  nome: "Reino Unido",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/250px-Flag_of_the_United_Kingdom.svg.png",
+  atributos: {
+    área: 242495,
+    população: 67886004,
+    densidade: 270.7,
+    IDH: 0.932,
+    renda: 46344
+  }
+};
+
+let carta8 = {
+  nome: "África do Sul",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/250px-Flag_of_South_Africa.svg.png",
+  atributos: {
+    área: 1221037,
+    população: 60142978,
+    densidade: 42.4,
+    IDH: 0.709,
+    renda: 5236
+  }
+};
+
+let carta9 = {
+  nome: "França",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/250px-Flag_of_France.svg.png",
+  atributos: {
+    área: 640679,
+    população: 67413000,
+    densidade: 116,
+    IDH: 0.901,
+    renda: 44995
+  }
+};
+
+let carta10 = {
+  nome: "Espanha",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/250px-Bandera_de_Espa%C3%B1a.svg.png",
+  atributos: {
+    área: 505990,
+    população: 47450795,
+    densidade: 94,
+    IDH: 0.904,
+    renda: 31178
+  }
+};
+
+let carta11 = {
+  nome: "China",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/250px-Flag_of_the_People%27s_Republic_of_China.svg.png",
+  atributos: {
+    área: 9596961,
+    população: 1411778724,
+    densidade: 145,
+    IDH: 0.761,
+    renda: 11819
+  }
+};
+
+let carta12 = {
+  nome: "Rússia",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/250px-Flag_of_Russia.svg.png",
+  atributos: {
+    área: 17098246,
+    população: 146171015,
+    densidade: 8.4,
+    IDH: 0.824,
+    renda: 11654
+  }
+};
+
+let carta13 = {
+  nome: "Índia",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/250px-Flag_of_India.svg.png",
+  atributos: {
+    área: 3287263,
+    população: 1352642280,
+    densidade: 412.5,
+    IDH: 0.645,
+    renda: 2191
+  }
+};
+
+let carta14 = {
+  nome: "Japão",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/250px-Flag_of_Japan.svg.png",
+  atributos: {
+    área: 377975,
+    população: 125360000,
+    densidade: 334,
+    IDH: 0.919,
+    renda: 42928
+  }
+};
+
+let carta15 = {
+  nome: "Coréia do Sul",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/250px-Flag_of_South_Korea.svg.png",
+  atributos: {
+    área: 100363,
+    população: 51709098,
+    densidade: 507,
+    IDH: 0.916,
+    renda: 34866
+  }
+};
+
+let carta16 = {
+  nome: "Turquia",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/250px-Flag_of_Turkey.svg.png",
+  atributos: {
+    área: 783356,
+    população: 83614362,
+    densidade: 109,
+    IDH: 0.82,
+    renda: 9327
+  }
+};
+
+let carta17 = {
+  nome: "Polônia",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Flag_of_Poland.svg/250px-Flag_of_Poland.svg.png",
+  atributos: {
+    área: 312696,
+    população: 38268000,
+    densidade: 123,
+    IDH: 0.88,
+    renda: 16930
+  }
+};
+
+let carta18 = {
+  nome: "Indonésia",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Flag_of_Indonesia.svg/250px-Flag_of_Indonesia.svg.png",
+  atributos: {
+    área: 1904569,
+    população: 270203917,
+    densidade: 141,
+    IDH: 0.718,
+    renda: 4256
+  }
+};
+
+let carta19 = {
+  nome: "Suíça",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Switzerland.svg/250px-Flag_of_Switzerland.svg.png",
+  atributos: {
+    área: 41285,
+    população: 8570146,
+    densidade: 207,
+    IDH: 0.955,
+    renda: 86673
+  }
+};
+
+let carta20 = {
+  nome: "México",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/250px-Flag_of_Mexico.svg.png",
+  atributos: {
+    área: 1972550,
+    população: 126014024,
+    densidade: 61,
+    IDH: 0.779,
+    renda: 10405
+  }
+};
+
+let carta21 = {
+  nome: "Colômbia",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/250px-Flag_of_Colombia.svg.png",
+  atributos: {
+    área: 1141748,
+    população: 50372424,
+    densidade: 42.23,
+    IDH: 0.767,
+    renda: 6744
+  }
+};
+
+let carta22 = {
+  nome: "Argentina",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/250px-Flag_of_Argentina.svg.png",
+  atributos: {
+    área: 2780400,
+    população: 45605826,
+    densidade: 14.4,
+    IDH: 0.845,
+    renda: 9890
+  }
+};
+
+let carta23 = {
+  nome: "Etiópia",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Flag_of_Ethiopia.svg/250px-Flag_of_Ethiopia.svg.png",
+  atributos: {
+    área: 1104300,
+    população: 117876227,
+    densidade: 92.7,
+    IDH: 0.485,
+    renda: 974
+  }
+};
+
+let carta24 = {
+  nome: "Nigéria",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Flag_of_Nigeria.svg/250px-Flag_of_Nigeria.svg.png",
+  atributos: {
+    área: 923769,
+    população: 211400708,
+    densidade: 218,
+    IDH: 0.539,
+    renda: 2432
+  }
+};
+
+let carta25 = {
+  nome: "Suécia",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Flag_of_Sweden.svg/250px-Flag_of_Sweden.svg.png",
+  atributos: {
+    área: 450295,
+    população: 10402070,
+    densidade: 25,
+    IDH: 0.945,
+    renda: 50339
+  }
+};
+
+let carta26 = {
+  nome: "Cingapura",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Flag_of_Singapore.svg/250px-Flag_of_Singapore.svg.png",
+  atributos: {
+    área: 728.6,
+    população: 5685800,
+    densidade: 7.804,
+    IDH: 0.938,
+    renda: 64103
+  }
+};
+
+let carta27 = {
+  nome: "Portugal",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/250px-Flag_of_Portugal.svg.png",
+  atributos: {
+    área: 92212,
+    população: 10347892,
+    densidade: 112.2,
+    IDH: 0.864,
+    renda: 25065
+  }
+};
+
+let carta28 = {
+  nome: "Egito",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/250px-Flag_of_Egypt.svg.png",
+  atributos: {
+    área: 1010408,
+    população: 101478581,
+    densidade: 100,
+    IDH: 0.707,
+    renda: 3561
+  }
+};
+
+let carta29 = {
+  nome: "Israel",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Flag_of_Israel.svg/250px-Flag_of_Israel.svg.png",
+  atributos: {
+    área: 20770,
+    população: 9407020,
+    densidade: 426,
+    IDH: 0.919,
+    renda: 44474
+  }
+};
+
+let carta30 = {
+  nome: "Serra Leoa",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Flag_of_Sierra_Leone.svg/250px-Flag_of_Sierra_Leone.svg.png",
+  atributos: {
+    área: 71740,
+    população: 8059155,
+    densidade: 112,
+    IDH: 0.452,
+    renda: 505
+  }
+};
+
+let carta31 = {
+  nome: "Árabia Saudita",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/250px-Flag_of_Saudi_Arabia.svg.png",
+  atributos: {
+    área: 2149690,
+    população: 34218169,
+    densidade: 15,
+    IDH: 0.854,
+    renda: 23566
+  }
+};
+
+let carta32 = {
+  nome: "Nicarágua",
+  imagem:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Nicaragua.svg/250px-Flag_of_Nicaragua.svg.png",
+  atributos: {
+    área: 21041,
+    população: 6825935,
+    densidade: 324.4,
+    IDH: 0.673,
+    renda: 4041
+  }
+};
+
+let cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22,
+  carta23,
+  carta24,
+  carta25,
+  carta26,
+  carta27,
+  carta28,
+  carta29,
+  carta30,
+  carta31,
+  carta32
+];
+
+var cartaMaquina = 0;
+
+var cartaJogador = 0;
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * cartas.length);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * cartas.length);
+
+  while (numeroCartaMaquina == numeroCartaJogador) {
+    numeroCartaJogador = parseInt(Math.random() * cartas.length);
+  }
+
+  cartaJogador = cartas[numeroCartaJogador];
+
+  var escolherAtributo = document.querySelector(".page-subtitle");
+  escolherAtributo.style.display = "block";
+
+  escolherAtributo.innerHTML = "Escolha o seu atributo";
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+
+  console.log(atributoSelecionado);
+
+  var elementoResultado = document.getElementById("resultado");
+
+  var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
+
+  console.log(valorCartaJogador);
+
+  if (valorCartaJogador == undefined) {
+    document.querySelector("#opcao-invalida").innerHTML =
+      "<br><p class='resultado-invalido'>Opção inválida</p>";
+    jogar();
+  }
+
+  var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+
+  console.log(valorCartaMaquina);
+
+  if (valorCartaMaquina < valorCartaJogador) {
+    document.querySelector("#opcao-invalida").innerHTML = "";
+    htmlResultado = "<br><p class='resultado-vencedor'>Você venceu!</p>";
+    document.querySelector("#carta-maquina").style.filter = "grayscale(1)";
+  } else if (valorCartaMaquina > valorCartaJogador) {
+    document.querySelector("#opcao-invalida").innerHTML = "";
+    htmlResultado = "<br><p class='resultado-perdedor'>Você perdeu...</p>";
+    document.querySelector("#carta-jogador").style.filter = "grayscale(1)";
+  } else if (valorCartaJogador == valorCartaMaquina) {
+    document.querySelector("#opcao-invalida").innerHTML = "";
+    htmlResultado = "<br><p class='resultado-empate'>Foi um empate!</p>";
+  }
+
+  elementoResultado.innerHTML = htmlResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+
+  exibirCartaMaquina();
+  document.getElementById("área").disabled = true;
+  document.getElementById("população").disabled = true;
+  document.getElementById("densidade").disabled = true;
+  document.getElementById("IDH").disabled = true;
+  document.getElementById("renda").disabled = true;
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.display = "block";
+
+  var bandeira = "<img class='img' src='" + cartaJogador.imagem + "'></div>";
+
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<ul class='atributoCarta'>" +
+      "<li><input required type='radio' name='atributo' value='" +
+      atributo +
+      "' id='" +
+      atributo +
+      "'>" +
+      "<label for='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "</label>" +
+      "</li>" +
+      "</ul>";
+  }
+  var nome = `
+  <div class="carta-container"><p class='carta-subtitle'>${cartaJogador.nome}</p>
+  `;
+
+  divCartaJogador.innerHTML =
+    nome + bandeira + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.display = "block";
+
+  var bandeira = "<img class='img' src='" + cartaMaquina.imagem + "'></div>";
+
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<ul class='atributoCarta'>" +
+      "<li><input type='radio' name='atributo' value='" +
+      atributo +
+      "'> " +
+      "<label for='atributo'" +
+      ">" +
+      atributo +
+      "</label> " +
+      cartaMaquina.atributos[atributo] +
+      "</li>" +
+      "</ul>";
+  }
+  var nome = `
+  <div class="carta-container"><p class='carta-subtitle'>${cartaMaquina.nome}</p>
+  `;
+
+  divCartaMaquina.innerHTML =
+    nome + bandeira + tagHTML + opcoesTexto + "</div>";
+}
+
+function reiniciar() {
+  var numeroCartaMaquina = "";
+  var numeroCartaJogador = "";
+  var reiniciarAtributo = document.querySelector(".page-subtitle");
+  reiniciarAtributo.innerHTML = "";
+  if (reiniciarAtributo.style.display !== "none") {
+    reiniciarAtributo.style.display = "none";
+  } else {
+    reiniciarAtributo.style.display = "block";
+  }
+  var reiniciarCartaJogador = document.getElementById("carta-jogador");
+  reiniciarCartaJogador.innerHTML = "";
+  if (reiniciarCartaJogador.style.display !== "none") {
+    reiniciarCartaJogador.style.display = "none";
+  } else {
+    reiniciarCartaJogador.style.display = "block";
+  }
+  var reiniciarCartaMaquina = document.getElementById("carta-maquina");
+  reiniciarCartaMaquina.innerHTML = "";
+  if (reiniciarCartaMaquina.style.display !== "none") {
+    reiniciarCartaMaquina.style.display = "none";
+  } else {
+    reiniciarCartaMaquina.style.display = "block";
+  }
+  document.querySelector("#opcao-invalida").innerHTML = "";
+  document.getElementById("resultado").innerHTML = "";
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("btnJogar").disabled = true;
+
+  document.querySelector("#carta-jogador").style.filter = "none";
+  document.querySelector("#carta-maquina").style.filter = "none";
+}
